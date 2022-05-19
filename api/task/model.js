1d15ad889fa20c2580fa1671e503b74989d7965e
unknown <ryancare@protonmail.com>
2022-05-19 16:03:05 -0500

    Added task model / router, for fully passing tests, but did the model without a join

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,37 @@
-// build your `Task` model here
+const db = require('../../data/dbConfig');
+
+async function getTasks() {
+ const tasksList = await db('tasks');
+ const projects = await db('projects');
+ const tasks = tasksList.map( task => {
+    const {task_id, task_notes, task_description, task_completed, project_id} = task;
+    const {project_name, project_description} = projects.filter( project => project.project_id === project_id)[0];
+    return {
+                task_id,
+                task_notes,
+                task_description,
+                task_completed: task_completed === 0 ? false : true,
+                project_name,
+                project_description
+            } 
+})
+ return tasks;
+ }
+
+ async function addTask(task) {
+    const taskId = await db('tasks').insert(task)
+    const newTask = await  db('tasks').where('task_id', taskId[0]);
+    const {task_id, task_notes, task_description, task_completed} = newTask[0];
+    return {
+        task_id,
+        task_notes,
+        task_description,
+        task_completed: task_completed === 0 ? false : true
+    }
+  }
+
+module.exports = {
+  getTasks,
+  addTask 
+  }
+  
--- api/task/router.js
+++ api/task/router.js
@@ -2,6 +2,17 @@ const express = require('express')
 const model = require('./model.js')
 
 const router = express.Router()
+router.get('/', (req, res, next) => {
+    model.getTasks()
+    .then(tasks => res.status(200).json(tasks))
+    .catch( err => next(err))
+})
+
+router.post('/', (req, res, next) => {
+    model.addTask(req.body)
+    .then(task => res.status(201).json(task))
+    .catch( err => next(err))
+})
 
 
 
