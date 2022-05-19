54ea0a1f1e9a2fe186d0a9550dffe4b9f693c2eb
unknown <ryancare@protonmail.com>
2022-05-19 13:41:18 -0500

    Wrote server.js, importing to-be-constructed routers and adding in json-parsing functionality middleware

--- api/server.js
+++ api/server.js
@@ -1,1 +1,16 @@
-// build your server here and require it from index.js
+const express = require('express');
+
+const ResourcesRouter = require('./resource/router.js');
+const ProjectsRouter = require('./project/router.js');
+const TasksRouter = require('./task/router.js');
+
+const server = express();
+
+
+server.use(express.json());
+
+server.use('/api/resources', ResourcesRouter);
+server.use('/api/projects', ProjectsRouter);
+server.use('/api/tasks', TasksRouter);
+
+module.exports = server;
